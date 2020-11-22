3912932f1a0d493cfcaac060145788797d70afde
adzh_ <56293503+adzh2100@users.noreply.github.com>
2020-11-22 12:12:14 +0200

    Update and rename Task-8.js to week-4/Task-8.js

---
+++ week-4/Task-8.js
@@ -0,0 +1,59 @@
+class PaymentMethod
+{
+	getAmount()
+  {
+  return "Your amount in the account is: ";
+  }
+}
+
+class CashMethod extends PaymentMethod
+{
+	constructor()
+  {
+    	super();
+  	var amount=0;
+	this.addToAmount(another_amount)
+   	{
+  	amount+=another_amount;
+  	}
+   	this.reduceFromAcmount(another_amount)
+   	{
+  	amount-=another_amount;
+   	} 
+ 	this.getAmount()
+   	{
+    	return `${PaymentMethod.getAmount()} ${amount}`;
+   	} 
+   }
+}
+
+class CreditMethod extends PaymentMethod
+{
+	constructor()
+  {
+    	super();
+  	var amount=0;
+ 	this.addToAmount(another_amount)
+   	{
+  	amount+=0.9*another_amount;
+  	}
+   	this.reduceFromAcmount(another_amount)
+   	{
+  	amount-=another_amount;
+   	} 
+ 	this.getAmount()
+   	{
+    	return `${PaymentMethod.getAmount()} ${amount}`;
+   	} 
+   }
+}
+
+const cashAccount = new CashMethod();
+cashAccount.getAmount(); // returns “Your amount in the account is: 0”
+cashAccount.addToAmount(100); // returns cashAccount instance (useful for method chaining)
+cashAccount.addToAmount(20).addToAmount(30).reduceFromAmount(10); // returns cashAccount instance (useful for method chaining)
+cashAccount.getAmount(); // returns “Your amount in the account is: 140”
+
+const creditAccount = new CreditMethod();
+creditAccount.addToAmount(100); // returns creditAccount instance (useful for method chaining)
+creditAccount.getAmount(); // returns “Your amount in the account is: 90”;
