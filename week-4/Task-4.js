156e41f6ae149420370c946adccd2bcf443825d7
adzh_ <56293503+adzh2100@users.noreply.github.com>
2020-11-22 10:29:05 +0200

    Create Task-4.js

---
+++ week-4/Task-4.js
@@ -0,0 +1,11 @@
+function person() 
+{
+			let _salary= 1000;
+			return function getSecretSalaryInfo()
+      {
+				return _salary;
+			}
+}
+
+		let stoleSalaryInfo = person();
+		console.log(stoleSalaryInfo()); //принтира 1000
